5f332b9e2283ca17ad9dbc3e4509e04473d83519
Benjamin Schütz <b.schuetz@familie-redlich.de>
2019-02-27 16:16:14 +0100

    Require typo3 7.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7-8.7.99',
+            'typo3' => '7.6-8.7.99',
         ],
         'conflicts' => [],
         'suggests' => [],
