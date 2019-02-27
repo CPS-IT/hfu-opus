92557b15a9adf063a69f7cf4796c0bf106fe2304
Benjamin Schütz <b.schuetz@familie-redlich.de>
2019-02-27 17:04:48 +0100

    Fix anchor links – Anchor links are linking to to startpage, if <base> tag is set with base url
    Prepend current url (current frontend uri) to anchor links.

--- Classes/ViewHelpers/PublistViewHelper.php
+++ Classes/ViewHelpers/PublistViewHelper.php
@@ -33,11 +33,58 @@ class PublistViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHel
     /**
      * @param string $publistid
      * @return mixed|string
+     * @throws \TYPO3\CMS\Core\Exception
      */
     public function render($publistid)
     {
         $settings = $this->getSettings();
+        $data = OPUS::fetchPubList($publistid, $settings['baseUrl']);
 
-        return OPUS::fetchPubList($publistid, $settings['baseUrl']);
+        // Parsing data
+        $data = $this->parseData($data);
+
+        return $data;
+    }
+
+    /**
+     * Parsing data
+     *
+     * @param string $data
+     * @return string
+     */
+    protected function parseData($data)
+    {
+        // Get current url
+        $uriBuilder = $this->controllerContext->getUriBuilder();
+        $currentUrl = $uriBuilder->buildFrontendUri();
+
+        // Parse data (html code) - add current url to anchor links
+        $data = $this->parseAnchorLinks($data, $currentUrl);
+
+        return $data;
+    }
+
+    /**
+     * Parsing anchor links - Add current url $currentUrl to anchor links within given data $data
+     *
+     * @param string $data
+     * @param string $currentUrl
+     * @return string
+     */
+    protected function parseAnchorLinks($data, $currentUrl)
+    {
+        if (preg_match_all('/(?<link><a[^>]*(?<attribute>href="(?<href>[^"]*)"[^>]*>))/is',
+                $data, $matches)
+            && isset($matches['link']) && !empty($matches['link'])
+        ) {
+            foreach ($matches['href'] as $matchKey => $matchLink) {
+                if (substr(trim($matchLink), 0, 1) === '#' || strpos($matchLink, '#') !== false) {
+                    $newLink = preg_replace('/' . $matchLink . '/', $currentUrl . $matchLink, $matches['link'][$matchKey]);
+                    $data = str_replace($matches['link'][$matchKey], $newLink, $data);
+                }
+            }
+        }
+
+        return $data;
     }
 }
