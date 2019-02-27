9adb242c3ae8fa3be7a06692119107adcf16e273
Benjamin Schütz <b.schuetz@familie-redlich.de>
2019-02-27 16:50:32 +0100

    Add url validator and clean up typoscript configuration

--- Classes/Utility/OpusApi.php
+++ Classes/Utility/OpusApi.php
@@ -2,6 +2,8 @@
 
 namespace Hfu\HfuOpus\Utility;
 
+use TYPO3\CMS\Core\Exception;
+
 /**
  * Class OpusApi
  *
@@ -17,12 +19,28 @@ class OpusApi
      */
     public static function fetchPubList($opusKey, $opusUrl)
     {
-
         // Kein Schlüssel wurde hinterlegt.
         if (empty($opusKey)) {
             return $opusKey;
         }
 
+        // Remove url from key if is set and build url
+        if (isset($opusUrl) && !empty($opusUrl)) {
+            $opusKey = preg_replace(
+                '/^' . addcslashes($opusUrl, '/') . '/',
+                '',
+                $opusKey
+            );
+        }
+        // Building url
+        $url = $opusUrl . $opusKey;
+
+        // Check url
+        if (!preg_match('/^https?:\/\//', $url)) {
+            throw new Exception('Given url to request OPUS data is not set correctly');
+        }
+
+        // Requesting data
         $options = array(
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_FOLLOWLOCATION => true,
@@ -30,7 +48,7 @@ class OpusApi
             CURLOPT_TIMEOUT => 5,
             CURLOPT_MAXREDIRS => 10,
         );
-        $ch = curl_init($opusUrl . $opusKey);
+        $ch = curl_init($url);
         curl_setopt_array($ch, $options);
         $data = curl_exec($ch);
         $errno = curl_errno($ch);
